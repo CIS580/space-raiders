0ae8c512a42fd3c0936bdc32a7d056dd781fe1f7
Matej Shanel <shanemat@ksu.edu>
2018-12-02 09:17:55 -0600

    Add object handling asset loading

---
+++ src/space/utils/assetLoader.js
@@ -0,0 +1,61 @@
+
+/** Path to folder assets are in */
+const ASSET_FOLDER = 'resources/space/';        // TODO: Figure out correct way how to include images
+
+/** Type of asset files */
+const ASSET_FILE_TYPE = '.png';
+
+/**
+ * @class AssetLoader
+ *
+ * A class providing means to load assets
+ */
+class AssetLoader {
+
+    /**
+     * Constructs object for loading assets
+     */
+    constructor() {
+        if (!AssetLoader.instance) {
+            AssetLoader.instance = this;
+
+            this.loadedImages = [];
+            this.preloadImages();
+        }
+
+        return AssetLoader.instance;
+    }
+
+    /**
+     * Preloads images needed
+     */
+    preloadImages() {
+        this.getAsset("starBackground");
+
+        // TODO: Preload required images
+    }
+
+    /**
+     * Retrieves asset specified by its name
+     *
+     * @param {String} name - name of asset to retrieve
+     * @returns Required asset
+     */
+    getAsset(name) {
+        if (this.loadedImages[name]) {
+            return this.loadedImages[name];
+        } else {
+            let image = new Image();
+            image.src = ASSET_FOLDER + name + ASSET_FILE_TYPE;
+
+            this.loadedImages[name] = image;
+            return image;
+        }
+    }
+}
+
+/** The only instance of this singleton */
+const assetLoader = new AssetLoader();
+Object.freeze(assetLoader);
+
+export default assetLoader;
