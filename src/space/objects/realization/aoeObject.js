eef412ffadc410214877ab2f883b04bd52860b82
Jozef Bugoš <bugosjoz@fel.cvut.cz>
2018-12-06 10:58:43 -0600

    fixed areColliding report, not fixed John's totally unhelpful PR review

--- src/space/objects/realization/aoeObject.js
+++ src/space/objects/realization/aoeObject.js
@@ -4,7 +4,7 @@ import Vector from "../../utils/vector";
 
 const SLOW_RADIUS = 300;
 const SLOW_FORCE = 10;
-const SLOW_VECTOR = new Vector(SLOW_FORCE,SLOW_FORCE);
+const SLOW_VECTOR = new Vector(SLOW_FORCE, SLOW_FORCE);
 
 export default class Slow extends EncounterObject {
     constructor(encounter, position) {
@@ -13,8 +13,8 @@ export default class Slow extends EncounterObject {
     }
 
     isContained(object) {
-        for(var i = 0; i < this.containing.length; i++) {
-            if(object === this.containing[i])
+        for (var i = 0; i < this.containing.length; i++) {
+            if (object === this.containing[i])
                 return true;
         }
         return false;
@@ -23,8 +23,8 @@ export default class Slow extends EncounterObject {
     render(elapsedTime, context) {
         context.save();
         context.beginPath();
-        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2);
-        context.strokeStyle="rgba(0, 0, 0, 0.1)";
+        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        context.strokeStyle = "rgba(0, 0, 0, 0.1)";
         context.fillStyle = 'rgba(0, 0, 0, 0.08)';
         context.fill();
         context.stroke();
@@ -36,14 +36,14 @@ export default class Slow extends EncounterObject {
     }
 
     areColliding(other) {
-        if(super.areColliding(other)) {
-            if(this.isContained(other)) {
+        if (other.areColliding(this)) {
+            if (this.isContained(other)) {
                 return false;
             }
             this.containing.push(other);
             return true;
         } else {
-            if(this.isContained(other)) {
+            if (this.isContained(other)) {
                 this.remove(other);
                 return false;
             }
