749b7112a7a7cfa3059321fb1d38dee97b4d791b
Jozef Bugoš <bugosjoz@fel.cvut.cz>
2018-12-05 22:32:11 -0600

    new AoE object; fixed colision detection and detection moved to objects

--- src/space/objects/realization/aoeObject.js
+++ src/space/objects/realization/aoeObject.js
@@ -7,15 +7,47 @@ const SLOW_FORCE = 10;
 const SLOW_VECTOR = new Vector(SLOW_FORCE,SLOW_FORCE);
 
 export default class Slow extends EncounterObject {
-    constructor(encounter, radius, position) {
+    constructor(encounter, position) {
         super(encounter, Type.SLOW, SLOW_RADIUS, position);
+        this.containing = [];
     }
 
-    collideWithShip(ship) {
-        ship.velocity = Vector.subtract(ship.velocity,SLOW_VECTOR);
+    isContained(object) {
+        for(var i = 0; i < this.containing.length; i++) {
+            if(object === this.containing[i])
+                return true;
+        }
+        return false;
     }
 
-    render() {
+    render(elapsedTime, context) {
+        context.save();
+        context.beginPath();
+        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2);
+        context.strokeStyle="rgba(0, 0, 0, 0.1)";
+        context.fillStyle = 'rgba(0, 0, 0, 0.08)';
+        context.fill();
+        context.stroke();
+    }
+
+    remove(element) {
+        const index = this.containing.indexOf(element);
+        this.containing.splice(index, 1);
+    }
+
+    areColliding(other) {
+        if(super.areColliding(other)) {
+            if(this.isContained(other)) {
+                return false;
+            }
+            this.containing.push(other);
+            return true;
+        } else {
+            if(this.isContained(other)) {
+                this.remove(other);
+                return false;
+            }
+        }
 
     }
 }
