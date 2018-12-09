274310ae8715375f398679a054b24c602f54d62b
Jozef Bugoš <bugosjoz@fel.cvut.cz>
2018-12-09 00:57:48 -0600

    Fixed bugs, changed to use angle for direction, now can create line of asteroids

--- src/space/objects/realization/asteroid.js
+++ src/space/objects/realization/asteroid.js
@@ -9,7 +9,8 @@ import MyMath from "../../utils/myMath";
 import AssetLoader from "../../utils/assetLoader";
 
 
-const AST_MAX_SPEED = 80;
+const AST_MAX_SPEED = 200;
+const AST_MIN_SPEED = 100;
 const AST_MIN_RADIUS = 30;
 const AST_MAX_RADIUS = 80;
 const ASSET = ["spaceMeteors_001","spaceMeteors_002","spaceMeteors_003","spaceMeteors_004"];
@@ -30,6 +31,7 @@ class Asteroid extends EncounterObject {
  * Factory of asteroids
  */
 export default class AsteroidCreator {
+
     /**
      * Creates one asteroid
      *
@@ -43,40 +45,73 @@ export default class AsteroidCreator {
     }
 
     /**
+     * Create offset vector for asteroid shower
+     * @param asteroid
+     * @param vertical
+     * @param horizontal
+     * @returns {Vector}
+     */
+    static offsetCalculator(asteroid,vertical,horizontal) {
+        let xOffset = vertical ? 0 : asteroid.radius * MyMath.randomBetween(0.6,1.2);
+        let yOffset = horizontal ? 0 : asteroid.radius * MyMath.randomBetween(0.6,1.2);
+        return new Vector(xOffset,yOffset);
+    }
+
+    /**
      *  Creates shower of asteroid spawned from specified position,
-     *  with specified direction either up left/ up right; or
-     *  down left/ down right
+     *  with specified direction
      *
      * @param encounter
      * @param position      from where asteroids should appear
      * @param count         how many asteroids should be spawned
-     * @param up            if true, asteroids go up; else they go down
-     * @param left          if true, asteroids go left; else they go right
+     * @param angle         angle of the direction in which asteroids go
+     * @param vertical      if true then create vertical line, if false create horizontal line, if undef, dost create line
      * @returns {Array}     array of instances to asteroid
      */
-    static createAsteroidShower(encounter, position, count, up, left) {
+    static createAsteroidShower(encounter, position, count, angle, vertical) {
         let asteroids = [];
+        let horizontal = vertical === undefined ? undefined : !vertical;
+        let angleTmp = angle;
+        let posTmp = new Vector(position.x,position.y);
         for(let i = 0; i < count; i++) {
-            let posTmp = new Vector(position.x,position.y);
-            let asteroid = new Asteroid(encounter,posTmp,MyMath.randomBetween(AST_MIN_RADIUS,AST_MAX_RADIUS));
-            if(up) {
-                asteroid.velocity.x *= (-1);
-                position.x += 10;
-            } else {
-                asteroid.velocity.x = encounter.width - asteroid.velocity.x;
-                position.x -= 10;
-            }
-            if(left) {
-                asteroid.velocity.y *= (-1);
-                position.y += 10;
-            } else {
-                asteroid.velocity.y = encounter.width - asteroid.velocity.y;
-                position.y -= 10;
-            }
-            asteroid.velocity = Vector.multiply(asteroid.velocity,0.1);
+            if(i !== 0)
+                posTmp.subtract(this.offsetCalculator(asteroids[i - 1],vertical, horizontal));
+            let asteroid = new Asteroid(encounter,new Vector(posTmp.x,posTmp.y),MyMath.randomBetween(AST_MIN_RADIUS,AST_MAX_RADIUS));
+            asteroid.velocity = (new Vector(Math.sin(angleTmp), -Math.cos(angleTmp))).multiply(MyMath.randomBetween(AST_MIN_SPEED,AST_MAX_SPEED));
             asteroids.push(asteroid);
+            angleTmp = angle * MyMath.randomBetween(0.85,1.16);
+            posTmp.subtract(this.offsetCalculator(asteroid,vertical, horizontal));
         }
         return asteroids;
+    }
+
+    /**
+     *  Creates shower of asteroid spawned from specified position,
+     *  with specified direction
+     *
+     * @param encounter
+     * @param position      from where asteroids should appear
+     * @param count         how many asteroids should be spawned
+     * @param angle         angle of the direction in which asteroids go
+     * @returns {Array}     array of instances to asteroid
+     */
+    static createPointAsteroidShower(encounter, position, count, angle) {
+        return this.createAsteroidShower(encounter,position,count,angle);
+    }
+
+    /**
+     *  Creates shower of asteroid spawned as either vertical or horizontal line
+     *  with specified direction
+     *
+     * @param encounter
+     * @param position      from where asteroids should appear
+     * @param count         how many asteroids should be spawned
+     * @param angle         angle of the direction in which asteroids go
+     * @param vertical      if true then create vertical line, if false create horizontal line
+     * @returns {Array}     array of instances to asteroid
+     */
+    static createLineAsteroidShower(encounter, position, count, angle, vertical) {
+        return this.createAsteroidShower(encounter,position,count,angle,vertical);
     }
 }
 
